629974ac4d25b402f96f548e8a701bff6507996e
Tyler Haddix <tyler.hadx@gmail.com>
2022-03-31 17:29:50 -0600

    another sw

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-    publicPath: '/BHMTNLION/'
+    publicPath: '/BHMTNLION/',
+    pwa: {
+      name: "my-pwa-app",
+      themeColor: "#624040",
+      msTileColor: "#000000",
+      // configure the workbox plugin
+      workboxPluginMode: "InjectManifest",
+      workboxOptions: {
+        // swSrc is required in InjectManifest mode.
+        swSrc: "service-worker.js", //path to your own service-worker file
+        // ...other Workbox options...
+      },
+    },
 }
