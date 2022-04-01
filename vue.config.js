b00454ef85a7259eb4ba0a52cb8aafaeec2d90d1
Tyler Haddix <tyler.hadx@gmail.com>
2022-03-31 18:15:04 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-    publicPath: '/BHMTNLION/',
-    pwa: {
-      name: "Black Hills Mountain Lion",
-      msTileColor: "#000000",
-      // configure the workbox plugin
-      workboxPluginMode: "InjectManifest",
-      workboxOptions: {
-        // swSrc is required in InjectManifest mode.
-        swSrc: "/src/registerServiceWworker.js", //path to your own service-worker file
-        // ...other Workbox options...
-      },
-    },
+    publicPath: '/BHMTNLION/',  
 }
